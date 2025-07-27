c70a0ee08d10732cfd27b38f0021d9bcbebd836a
JonnyCoker <jonnyAcoker@gmail.com>
2025-07-27 16:44:53 -0600

    Create jerry.py

---
+++ jerry.py
@@ -0,0 +1,28 @@
+print("hello world") 
+
+print("find the min age you could date without being a creep like jerry")
+
+
+
+age = int(input("How old are you? "))
+
+minAge = age / 2  + 7
+
+print(" you can date someone as young as ") 
+print(minAge) 
+print(" without being a total fucking creep like jerry")
+
+
+
+# print("user input: ")
+# userInput = input()
+
+
+# statement = input("user prompt: Jerry Seinfeld is complicit with genocide. ")
+
+# print("statement variable: " + statement)
+# print("user Input: " + userInput)
+
+
+
+ 
